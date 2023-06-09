c135d7e0b12d54cf367c7e1607f84f4fd286b95b
Jeremie Bahati <jeremiahbht@gmail.com>
2023-06-09 13:51:14 +0200

    alx low level programming

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,15 +8,18 @@ int main(int argc __attribute__((unused/**
 						       *
 						       */)
 {
-	if (argc != 3)
-	{
-		printf("error\n");
-		return (1);
-}
-
-int num1 = atoi(argv[1]);
+	int num1 = atoi(argv[1]);
 int num2 = atoi(argv[2]);
 int result = num1 * num2;
+{
 printf("%d\n", result);
+}
+{
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
 return (0);
+}
 }
